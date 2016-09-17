4710ad090b4c00fad5fe172a4d3c8ae8833bfbdb
chp1995 <405593951@qq.com>
2016-09-17 21:36:25 +0800

    w

--- word service.py
+++ word service.py
@@ -2,12 +2,11 @@
 import time, socket, threading
 
 def link(sock, addr):
-    #print 'Accept new connection from %s:%s...' % addr
     sock.send('Hello!')
     while True:
         data = sock.recv(1024)
         time.sleep(1)
-        #如果客户端发送了exit字符串，就直接关闭连接
+        #发送exit字符串关闭连接
         if data == 'exit' or not data:
             break
         data.reverse()
@@ -22,8 +21,7 @@ s.bind(('127.0.0.1', 3333))
 s.listen(5)
 print 'Waiting for connection...'
 while True:
-    # 接受一个新连接:
     sock, addr = s.accept()
-    # 创建新线程来处理TCP连接:
+    # 创建新线程:
     t = threading.Thread(target=link, args=(sock, addr))
     t.start()
